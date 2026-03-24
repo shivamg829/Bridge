8dd04acb43395c1c374dcea867e0fbbb88bc6014
shivamg829 <shivamgangwar2197@gmail.com>
2026-03-24 18:51:48 +0530

    A router is added for get all chats and new model added for msg

--- backend/controllers/chat.controller.js
+++ backend/controllers/chat.controller.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Chat = require("../models/chat.model");
 const authMiddleware = require("../middleware/auth.middleware");
 
-router.post("/create-new-chats", authMiddleware, async (req, res) => {
+router.post("/create-new-chat", authMiddleware, async (req, res) => {
   try {
     const chat = new Chat(req.body);
     const savedChat = await chat.save();
@@ -16,6 +16,23 @@ router.post("/create-new-chats", authMiddleware, async (req, res) => {
       message: error.message,
       success: false,
     });
+  }
+});
+router.post("/get-all-chats", authMiddleware, async (req, res) => {
+  try {
+    const allChats = await Chat.find({
+      members: { $in: req.body.userId },
+    });
+    res.status(201).send({
+      message: "Chat fetch successfully",
+      success: true,
+      data: allChats
+    })
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+      success: false
+    })
   }
 });
 
