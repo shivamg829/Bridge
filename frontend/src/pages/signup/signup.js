b252d6c478f62a1ef977f76625a8fb94dea7611e
shivamg829 <shivamgangwar2197@gmail.com>
2026-03-26 19:27:51 +0530

    SignUp page done

--- frontend/src/pages/signup/signup.js
+++ frontend/src/pages/signup/signup.js
@@ -1,4 +1,43 @@
-function SignUp(){
-    return <h1>SignUP</h1>
+import React from "react";
+function SignUp() {
+    const [user, setUser] = React.useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    });
+    function onFormSubmit(e){
+        e.preventDefault();
+        console.log(user);
+    }
+    return (
+        <div className="container">
+            <div className="container-back-img"></div>
+            <div className="container-back-color"></div>
+            <div className="card">
+                <div className="card_title">
+                    <h1>Create Account</h1>
+                </div>
+                <div className="form">
+                    <form onSubmit={onFormSubmit}>
+                        <div className="column">
+                            <input type="text" placeholder="First Name" value={user.firstname} onChange={(e)=>setUser({...user, firstName: e.target.value})}/>
+                            <input type="text" placeholder="Last Name"  value={user.lastName} onChange={(e)=>setUser({...user, lastName: e.target.value})}/>
+                        </div>
+                        <input type="email" placeholder="Email" value={user.email} onChange={(e)=>setUser({...user, email: e.target.value})}/>
+                        <input type="password" placeholder="Password" value={user.password} onChange={(e)=>setUser({...user, password: e.target.value})}/>
+                        <button type="submit">Sign Up</button>
+                    </form>
+                </div>
+                <div className="card_terms">
+                    <span>
+                        Already have an account?
+                        <a>Login Here</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 }
-export default SignUp
+
+export default SignUp;
