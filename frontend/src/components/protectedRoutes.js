54bb4b66aead65cdbd38e64c00f698bd5b92df53
shivamg829 <shivamgangwar2197@gmail.com>
2026-04-27 01:40:24 +0530

    Fetch the details of currently logged in user

--- frontend/src/components/protectedRoutes.js
+++ frontend/src/components/protectedRoutes.js
@@ -1,16 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getLoggedUser} from './../api/users';
 function ProtectedRoute({children}){ {
+    const [user, setUser] = useState(null);
     const navigate = useNavigate();
+    const getLoggedInUser = async () =>{
+        let response = null;
+        try {
+            response = await getLoggedUser();
+            if(response.success){
+                setUser(response.data);
+            }else{
+                navigate('/login')
+            }
+        } catch (error) {
+            navigate('/login')
+            
+        }
+    }
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
-            
+            getLoggedInUser();
         }else{
-            Navigate('/login');
+            navigate('/login');
         }
     });
     <div>
+        <p>Name: {user?.firstName + ' ' + user?.lastName}</p>
         {children}
     </div>
 }}
