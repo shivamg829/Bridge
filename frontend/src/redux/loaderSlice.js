7b0b9136023c1d5d6dbe01900c153c08355cab39
shivamg829 <shivamgangwar2197@gmail.com>
2026-04-28 01:47:35 +0530

    loaderSlice and store created

--- frontend/src/redux/loaderSlice.js
+++ frontend/src/redux/loaderSlice.js
@@ -1,12 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 const loaderSlice = createSlice({
     name: 'loader',
-    initialState: {
-        isLoading: false,
-    },  
+    initialState: false,  
     reducers: {
-        setLoading: (state, action) => {
-            state.isLoading = action.payload;
+        showLoader: (state) => {
+            state.loader = true;
         },
+        hideLoader: (state) => {
+            state.loader = false;
+        }
     },
 });
+
+export const { showLoader, hideLoader } = loaderSlice.actions;
+export default loaderSlice.reducer;
